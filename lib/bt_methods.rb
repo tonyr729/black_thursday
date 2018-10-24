c5a95f28657563f40cac0de97b14d9f4833964e6
Tony Robertson <tonyr729@gmail.com>
2018-10-24 16:47:16 -0600

    Add method to module to find by name using helper method

--- lib/bt_methods.rb
+++ lib/bt_methods.rb
@@ -8,5 +8,18 @@ module BTMethods
       repository_element.id == id
     end
   end
+  
+  def find_by_name(name)
+    where(name, "name")
+  end
 
+  def where(value, key)
+    @repository.find do |repository_element|
+      method_name = key
+      property = repository_element.public_send(method_name) if repository_element.respond_to? method_name
+      property == value
+    end
+  end
+
+  
 end
