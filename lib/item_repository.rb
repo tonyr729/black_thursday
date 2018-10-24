419d98ff7923f08120f681aedc40dcbac04398da
abenetka <ali.benetka@gmail.com>
2018-10-24 12:31:21 -0600

    Adds implementation for item repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -1,4 +1,16 @@
 class ItemRepository
+  attr_reader :items
+
+  def initialize(items)
+    @items = items
+  end
+
+  def self.create_items(items_data)
+    item_list = items_data.map do |item_data|
+      Item.new(item_data)
+    end
+    self.new(item_list)
+  end
 
 
 
