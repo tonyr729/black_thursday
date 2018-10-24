36cfce7d19b29489282683ecd2e77de818a08444
Tony Robertson <tonyr729@gmail.com>
2018-10-23 18:03:47 -0600

    Add implementation for create_merchants test

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -4,4 +4,16 @@ class MerchantRepository
   def initialize(merchants)
     @merchants = merchants
   end
+
+  def self.create_merchants(merchants_data)
+    merchant_list = merchants_data.map do |merchant_data|
+      Merchant.new({
+        id: merchant_data[:id], 
+        name: merchant_data[:name]
+      })
+
+    end
+    
+    self.new(merchant_list)
+  end
 end
