45bc96bf70ea8ab4bd95dee8af87cc88a5401fe3
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-27 13:32:04 -0600

    Adds method 2 to sales_analyst; may need refactor later

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 class SalesAnalyst
 
@@ -9,7 +10,23 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    @items.repository.count / @merchants.repository.count.to_f
+    x = @items.repository.count / @merchants.repository.count.to_f
+    x.round(2)
   end
+
+  def average_items_per_merchant_standard_deviation
+    sorted_items = @items.repository.sort_by {|item| item.merchant_id}
+    merchant_ids = sorted_items.map {|item| item.merchant_id}
+    grouped_hash = merchant_ids.group_by {|id| id}
+    number_of_items_per_merchant = grouped_hash.map {|k,v| v.count}
+    square = number_of_items_per_merchant.map do |num|
+      x = (num - average_items_per_merchant) ** 2
+      x.abs
+    end
+    summed_squares = square.inject(0) { |sum, x| sum + x }
+    st_dev = Math.sqrt(summed_squares / (square.count - 1))
+    st_dev.round(2)
+  end
+
 
 end
