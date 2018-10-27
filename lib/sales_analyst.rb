bc41f3773395dca6cf1560b0cb1b3b98ba7388f0
SyntheticAutomation <mclampet@msudenver.edu>
2018-10-26 18:26:40 -0600

    First attempts on avg_items_per_merchant method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -1,5 +1,16 @@
-class SalesAnalyst
+require_relative '../lib/sales_engine'
 
 
+class SalesAnalyst < SalesEngine
+
+  attr_reader :items, :merchants
+
+  def initialize(merchants, items)
+    super(merchants, items)
+  end
+
+  def average_items_per_merchant
+    (@items.count / @merchants.count.to_f)
+  end
 
 end
