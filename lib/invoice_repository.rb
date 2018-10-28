5b6713c6ba89a9a0f2bf36176f31d1f555ded8c7
Tony Robertson <tonyr729@gmail.com>
2018-10-27 21:24:24 -0600

    Add implementation of tests for invoice repository

---
+++ lib/invoice_repository.rb
@@ -0,0 +1,30 @@
+require_relative '../lib/bt_methods'
+class InvoiceRepository
+  include BTMethods
+  attr_reader :repository
+
+  def initialize(invoices)
+    @repository = invoices
+    @new_instance = Invoice
+  end
+  
+  def self.create_invoices(invoices_data)
+    invoice_list = invoices_data.map do |invoice_data|
+      Invoice.new(invoice_data)
+    end
+
+    self.new(invoice_list)
+  end
+
+  def find_all_by_customer_id(customer_id)
+    where_any(customer_id, "customer_id")
+  end
+
+  def find_all_by_merchant_id(merchant_id)
+    where_any(merchant_id, "merchant_id")
+  end
+
+  def find_all_by_status(status)
+    where_any(status, "status")
+  end
+end
