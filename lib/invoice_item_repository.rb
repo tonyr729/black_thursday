cb4087bea12b43ead2d09dce6d275c747d9e23ed
Tony Robertson <tonyr729@gmail.com>
2018-10-29 17:11:06 -0600

    Add implementation for invoice_item_repository test

---
+++ lib/invoice_item_repository.rb
@@ -0,0 +1,28 @@
+require_relative '../lib/bt_methods'
+require 'bigdecimal'
+
+class InvoiceItemRepository
+  include BTMethods
+  attr_reader :repository
+
+  def initialize(items)
+    @repository = items
+    @new_instance = InvoiceItem
+  end
+
+  def self.create_invoice_items(invoice_items_data)
+    invoice_item_list = invoice_items_data.map do |invoice_item_data|
+      InvoiceItem.new(invoice_item_data)
+    end
+    self.new(invoice_item_list)
+  end
+
+  def find_by_item_id(item_id)
+    where_any(item_id, "item_id")
+  end
+
+  def inspect
+    "#<#{self.class} #{@repository.size} rows>"
+  end
+
+end
