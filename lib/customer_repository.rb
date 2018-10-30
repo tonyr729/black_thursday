db8426adb1c51e6a60d21fca019617b6ad2b72bb
Tony Robertson <tonyr729@gmail.com>
2018-10-29 19:29:30 -0600

    Add implementation for customer_repository test

---
+++ lib/customer_repository.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/bt_methods'
+
+class CustomerRepository
+  include BTMethods
+  attr_reader :repository
+
+  def initialize(customer)
+    @repository = customer
+    @new_instance = Customer
+  end
+
+  def self.create_customers(customers_data)
+    customer_list = customers_data.map do |customer_data|
+      Customer.new(customer_data)
+    end
+    self.new(customer_list)
+  end
+
+  def find_by_first_name(name)
+    where(name, "first_name")
+  end
+
+  def find_by_last_name(name)
+    where(name, "last_name")
+  end
+
+  def inspect
+    "#<#{self.class} #{@repository.size} rows>"
+  end
+
+end
